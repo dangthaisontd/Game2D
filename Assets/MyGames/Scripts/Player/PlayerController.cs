d83a0a10e88f5b341af494abb73ab6e2222986b5
dangthaisontd <dangthaisontd@gmail.com>
2024-12-05 21:32:29 +0700

    add
    add

---
+++ Assets/MyGames/Scripts/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+
+
+[AddComponentMenu("DangSon/PlayerController")]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    private Rigidbody2D rb;
+    public float jumFore=5.0f;
+    [SerializeField] private Transform groundCheck;
+    public LayerMask groundLayer;
+    public float radius = 0.5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(horizontal * moveSpeed, rb.linearVelocity.y);
+        //
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jum();
+        }
+    }
+    bool IsGround()
+    {
+        bool isLocalGround = Physics2D.OverlapCircle(groundCheck.position, radius, groundLayer);
+        return isLocalGround;
+    }
+    void Jum()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumFore);
+    }
+}
